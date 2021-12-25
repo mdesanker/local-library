7d9a3aa703e733de73ee23f6ecc2b72f18d7567e
Michael Desanker <mdesanker@gmail.com>
2021-12-25 14:46:12 -0500

    Updated author model and ran script to populate db

--- models/author.js
+++ models/author.js
@@ -3,19 +3,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
-  first_name: { type: String, required: true, maxlength: 100 },
-  last_name: { type: String, required: true, maxlength: 100 },
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
   date_of_birth: { type: Date },
   date_of_death: { type: Date },
 });
 
 // Virtual for author's full name
-AuthorSchema.virtual("name").get(() => {
+AuthorSchema.virtual("name").get(function () {
   const fullname = "";
-  if (this.first_name && this.last_name) {
-    fullname = `${this.last_name}, ${this.first_name}`;
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ", " + this.first_name;
   }
-  if (!this.first_name || !this.last_name) {
+  if (!this.first_name || !this.family_name) {
     fullname = "";
   }
   return fullname;
