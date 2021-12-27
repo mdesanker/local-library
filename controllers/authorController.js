e7afa644b89ff3b75744b3e2a6c632ab996e9cbe
Michael Desanker <mdesanker@gmail.com>
2021-12-27 12:02:39 -0500

    display author detail page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,7 @@
 const Author = require("../models/author");
+const Book = require("../models/book");
+
+const async = require("async");
 
 // Display list of all authors
 exports.author_list = (req, res) => {
@@ -18,7 +21,33 @@ exports.author_list = (req, res) => {
 
 // Display detail page for a specific author
 exports.author_detail = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author detail: " + req.params.id);
+  async.parallel(
+    {
+      author: function (callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books: function (callback) {
+        Book.find({ author: req.params.id }, "title summary").exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      } // Error in API usage.
+      if (results.author == null) {
+        // No results.
+        var err = new Error("Author not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render.
+      res.render("author_detail", {
+        title: "Author Detail",
+        author: results.author,
+        author_books: results.authors_books,
+      });
+    }
+  );
 };
 
 // Display author create form on GET
