8040b989460833040a0fc7d0aae3be323e7b7484
Michael Desanker <mdesanker@gmail.com>
2021-12-27 12:05:07 -0500

    Display bookinstance detail page

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -16,8 +16,8 @@ const BookInstanceSchema = new Schema({
 });
 
 // Virtual for bookinstance's URL
-BookInstanceSchema.virtual("url").get(() => {
-  return `/catalog/bookinstance/${this._id}`;
+BookInstanceSchema.virtual("url").get(function () {
+  return "/catalog/bookinstance/" + this._id;
 });
 
 // Virtual for due back date
