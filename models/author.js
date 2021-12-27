7ab7b22322332b3c389a3ad7e607f40bd5703b19
Michael Desanker <mdesanker@gmail.com>
2021-12-27 08:52:23 -0500

    Display author list with formatted lifespan dates

--- models/author.js
+++ models/author.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
@@ -39,6 +40,20 @@ AuthorSchema.virtual("lifespan").get(function () {
 // Virtual for author's URL
 AuthorSchema.virtual("url").get(function () {
   return "/catalog/author/" + this._id;
+});
+
+// Virtual for author's birth date formatted
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : "";
+});
+
+// Virtual for author's birth date formatted
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : "";
 });
 
 //Export model
