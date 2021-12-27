ad785fafc9ce63b155700d763266bcdc936e17e5
Michael Desanker <mdesanker@gmail.com>
2021-12-27 08:18:56 -0500

    Format due dates with luxon

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -17,6 +18,11 @@ const BookInstanceSchema = new Schema({
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual("url").get(() => {
   return `/catalog/bookinstance/${this._id}`;
+});
+
+// Virtual for due back date
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 // Export model
